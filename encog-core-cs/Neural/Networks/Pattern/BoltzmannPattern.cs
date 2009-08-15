691917f745ccda298047ed3a2d5a6e1f93f2c4b9
Jeff Heaton <jeff@jeffheaton.com>
2009-08-15 02:28:55 +0000

    Adding BoltzmannPattern
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@88 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

---
+++ encog-core-cs/Neural/Networks/Pattern/BoltzmannPattern.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using Encog.Neural.Activation;
+using Encog.Neural.Networks.Layers;
+using Encog.Neural.Networks.Logic;
+
+namespace Encog.Neural.Networks.Pattern
+{
+    /// <summary>
+    /// Pattern to create a Boltzmann machine.
+    /// </summary>
+    public class BoltzmannPattern : INeuralNetworkPattern
+    {
+
+        /// <summary>
+        /// The logging object.
+        /// </summary>
+        private readonly ILog logger = LogManager.GetLogger(typeof(ADALINEPattern));
+
+        /// <summary>
+        /// The number of neurons in the Boltzmann network.
+        /// </summary>
+        private int neuronCount;
+
+        /// <summary>
+        /// The number of annealing cycles per run.
+        /// </summary>
+        private int annealCycles = 100;
+
+        /// <summary>
+        /// The number of cycles per run.
+        /// </summary>
+        private int runCycles = 1000;
+
+        /// <summary>
+        /// The current temperature.
+        /// </summary>
+        private double temperature = 0.0;
+
+        /// <summary>
+        /// Not supported, will throw an exception, Boltzmann networks have
+        /// no hidden layers.
+        /// </summary>
+        /// <param name="count">Not used.</param>
+        public void AddHiddenLayer(int count)
+        {
+            String str = "A Boltzmann network has no hidden layers.";
+            if (this.logger.IsErrorEnabled)
+            {
+                this.logger.Error(str);
+            }
+            throw new PatternError(str);
+        }
+
+        /// <summary>
+        /// Clear any properties set on this network.
+        /// </summary>
+        public void Clear()
+        {
+            this.neuronCount = 0;
+
+        }
+
+        /// <summary>
+        /// Generate the network.
+        /// </summary>
+        /// <returns>The generated network.</returns>
+        public BasicNetwork Generate()
+        {
+            ILayer layer = new BasicLayer(new ActivationBiPolar(), true,
+                    this.neuronCount);
+
+            BasicNetwork result = new BasicNetwork(new BoltzmannLogic());
+            result.SetProperty(BoltzmannLogic.PROPERTY_ANNEAL_CYCLES, this.annealCycles);
+            result.SetProperty(BoltzmannLogic.PROPERTY_RUN_CYCLES, this.runCycles);
+            result.SetProperty(BoltzmannLogic.PROPERTY_TEMPERATURE, this.temperature);
+            result.AddLayer(layer);
+            layer.AddNext(layer);
+            layer.X = PatternConst.START_X;
+            layer.Y = PatternConst.START_Y;
+            result.Structure.FinalizeStructure();
+            result.Reset();
+            return result;
+        }
+
+        /// <summary>
+        /// Not used, will throw an exception.
+        /// </summary>
+        public IActivationFunction ActivationFunction
+        {
+            get
+            {
+                String str =
+                    "A Boltzmann network will use the BiPolar activation "
+                    + "function, no activation function needs to be specified.";
+                if (this.logger.IsErrorEnabled)
+                {
+                    this.logger.Error(str);
+                }
+                throw new PatternError(str);
+            }
+            set
+            {
+                String str =
+                    "A Boltzmann network will use the BiPolar activation "
+                    + "function, no activation function needs to be specified.";
+                if (this.logger.IsErrorEnabled)
+                {
+                    this.logger.Error(str);
+                }
+                throw new PatternError(str);
+            }
+        }
+
+        /// <summary>
+        /// Number of neurons.
+        /// </summary>
+        public int OutputNeurons
+        {
+            set
+            {
+                this.neuronCount = value;
+            }
+            get
+            {
+                return this.neuronCount;
+            }
+        }
+
+        /// <summary>
+        /// Number of neurons.
+        /// </summary>
+        public int InputNeurons
+        {
+            set
+            {
+                this.neuronCount = value;
+            }
+            get
+            {
+                return this.neuronCount;
+            }
+        }
+
+        /// <summary>
+        /// The number of anneal cycles.
+        /// </summary>
+        public int AnnealCycles
+        {
+            get
+            {
+                return this.annealCycles;
+            }
+            set
+            {
+                this.annealCycles = value;
+            }
+        }
+
+
+        /// <summary>
+        /// The number of run cycles.
+        /// </summary>
+        public int RunCycles
+        {
+            get
+            {
+                return this.runCycles;
+            }
+            set
+            {
+                this.runCycles = value;
+            }
+        }
+
+        /// <summary>
+        /// The temperature.
+        /// </summary>
+        public double Temperature
+        {
+            get
+            {
+                return this.temperature;
+            }
+            set
+            {
+                this.temperature = value;
+            }
+        }
+
+    }
+
+}
