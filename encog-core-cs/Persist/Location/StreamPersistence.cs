93c878fea45787c9425804bd15532e9625463da8
Jeff Heaton <jeff@jeffheaton.com>
2009-09-16 21:35:05 +0000

    Added StreamPersistence
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@112 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

---
+++ encog-core-cs/Persist/Location/StreamPersistence.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Encog.Persist.Location
+{
+    /**
+     * Create a location based on a Stream.
+     */
+    public class StreamPersistence : PersistenceLocation
+    {
+#if logging
+        /// <summary>
+        /// The logging object.
+        /// </summary>
+        private readonly ILog logger = LogManager.GetLogger(typeof(FilePersistence));
+#endif
+        /// <summary>
+        /// The stream to persist to/from.
+        /// </summary>
+        private Stream stream;
+
+        /// <summary>
+        /// Construct a persistance location based on a stream.
+        /// </summary>
+        /// <param name="stream">The stream to use.</param>
+        public StreamPersistence(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        /// <summary>
+        /// The stream to use.
+        /// </summary>
+        /// <returns>A new InputStream for this file.</returns>
+        public Stream CreateStream(FileMode mode)
+        {
+            return this.stream;
+        }
+
+        /// <summary>
+        /// Attempt to delete the file.  Will fail for this location type.
+        /// </summary>
+        public void Delete()
+        {
+        }
+
+        /// <summary>
+        /// Does the file exist?  This is always true for this location type.
+        /// </summary>
+        /// <returns>True if the file exists.</returns>
+        public bool Exists()
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Rename this file to a different location.
+        /// </summary>
+        /// <param name="toLocation">What to rename to.</param>
+        public void RenameTo(PersistenceLocation toLocation)
+        {
+                String str =
+                   "Rename is not supported on this location type.";
+#if logging
+                if (this.logger.IsErrorEnabled)
+                {
+                    this.logger.Error(str);
+                }
+#endif
+                throw new PersistError(str);
+ 
+        }
+    }
+}
