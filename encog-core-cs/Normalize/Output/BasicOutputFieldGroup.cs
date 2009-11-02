01c251b73b1c0a0f8a649688066a47ad51ab08e7
Jeff Heaton <jeff@jeffheaton.com>
2009-11-02 00:39:18 +0000

    Added more norm stuff
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@132 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

---
+++ encog-core-cs/Normalize/Output/BasicOutputFieldGroup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Encog.Persist.Attributes;
+
+namespace Encog.Normalize.Output
+{
+    /// <summary>
+    /// Provides very basic functionality that other output field groups
+    /// will use.  Mainly provides the list of fields that are grouped.
+    /// </summary>
+    public abstract class BasicOutputFieldGroup : IOutputFieldGroup
+    {
+        /// <summary>
+        /// The fields in this group.
+        /// </summary>
+        [EGReference]
+        private ICollection<OutputFieldGrouped> fields = new List<OutputFieldGrouped>();
+
+        /// <summary>
+        /// Add a field to this group.
+        /// </summary>
+        /// <param name="field">The field to add to the group.</param>
+        public void AddField(OutputFieldGrouped field)
+        {
+            this.fields.Add(field);
+        }
+
+        /// <summary>
+        /// The list of grouped fields.
+        /// </summary>
+        public ICollection<OutputFieldGrouped> GroupedFields
+        {
+            get
+            {
+                return this.fields;
+            }
+        }
+
+        /// <summary>
+        /// Init for a new row.
+        /// </summary>
+        public abstract void RowInit();
+    }
+}
---
+++ encog-core-cs/Normalize/Output/Mapped/MappedRange.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Encog.Persist.Attributes;
+
+namespace Encog.Normalize.Output.Mapped
+{
+    /// <summary>
+    /// Simple class that is used internally to hold a range mapping.
+    /// </summary>
+    public class MappedRange
+    {
+        /// <summary>
+        /// The low value for the range.
+        /// </summary>
+        [EGAttribute]
+        private double low;
+
+        /// <summary>
+        /// The high value for the range.
+        /// </summary>
+        [EGAttribute]
+        private double high;
+
+        /// <summary>
+        /// The value that should be returned for this range.
+        /// </summary>
+        [EGAttribute]
+        private double value;
+
+        /// <summary>
+        /// Construct the range mapping.
+        /// </summary>
+        /// <param name="low">The low value for the range.</param>
+        /// <param name="high">The high value for the range.</param>
+        /// <param name="value">The value that this range represents.</param>
+        public MappedRange(double low, double high, double value)
+        {
+            this.low = low;
+            this.high = high;
+            this.value = value;
+        }
+
+        /// <summary>
+        /// The high value for this range.
+        /// </summary>
+        public double High
+        {
+            get
+            {
+                return this.high;
+            }
+        }
+
+        /// <summary>
+        /// The low value for this range.
+        /// </summary>
+        public double Low
+        {
+            get
+            {
+                return this.low;
+            }
+        }
+
+        /// <summary>
+        /// The value that this range represents.
+        /// </summary>
+        public double Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
+
+        /// <summary>
+        /// Determine if the specified value is in the range.
+        /// </summary>
+        /// <param name="d">The value to check.</param>
+        /// <returns>True if this value is within the range.</returns>
+        public bool InRange(double d)
+        {
+            if ((d >= this.low) && (d <= this.high))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
