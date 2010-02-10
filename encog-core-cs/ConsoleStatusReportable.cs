5b3feeffe3112641689c66a8f8915d701dd59147
Jeff Heaton <jeff@jeffheaton.com>
2010-02-10 13:03:23 +0000

    Additions for 2.3
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@208 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

---
+++ encog-core-cs/ConsoleStatusReportable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog
+{
+    /// <summary>
+    /// A simple status report that goes to the console.
+    /// </summary>
+    public class ConsoleStatusReportable : IStatusReportable
+    {
+        /// <summary>
+        /// Simply display any status reports.
+        /// </summary>
+        /// <param name="total">Total amount.</param>
+        /// <param name="current">Current item.</param>
+        /// <param name="message">Current message.</param>
+        public void Report(int total, int current,
+                String message)
+        {
+            if (total == 0)
+            {
+                Console.WriteLine(current + " : " + message);
+            }
+            else
+            {
+                Console.WriteLine(current + "/" + total + " : " + message);
+            }
+
+        }
+    }
+}
