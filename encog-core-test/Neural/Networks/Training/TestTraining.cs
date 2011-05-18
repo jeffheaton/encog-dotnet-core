02c849a6725b8241eabcdaae54093f35051739ca
Jeff Heaton <jeff@jeffheaton.com>
2011-05-18 12:13:47 +0000

    Added unit testss
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@656 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

---
+++ encog-core-test/Neural/Networks/Training/TestTraining.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Encog.ML.Data;
+using Encog.ML.Data.Basic;
+using Encog.Util;
+using Encog.ML.Train;
+using Encog.Neural.Networks.Training.Propagation.Resilient;
+using Encog.Neural.Networks.Training.Lma;
+using Encog.Neural.Networks.Training.Propagation.Back;
+using Encog.Neural.Networks.Training.Propagation.Manhattan;
+using Encog.Neural.Networks.Training.Propagation.SCG;
+using Encog.Neural.Networks.Training.Anneal;
+using Encog.Neural.Networks.Training.Genetic;
+using Encog.MathUtil.Randomize;
+using Encog.Neural.PNN;
+using Encog.Neural.Networks.Training.PNN;
+
+namespace Encog.Neural.Networks.Training
+{
+    [TestClass]
+    public class TestTraining
+    {
+        [TestMethod]
+        public void TestRPROP()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+            MLTrain rprop = new ResilientPropagation(network, trainingData);
+            NetworkUtil.TestTraining(rprop, 0.03);
+        }
+
+        [TestMethod]
+        public void TestLMA()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+            MLTrain rprop = new LevenbergMarquardtTraining(network, trainingData);
+            NetworkUtil.TestTraining(rprop, 0.03);
+        }
+
+        [TestMethod]
+        public void TestBPROP()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+
+            MLTrain bprop = new Backpropagation(network, trainingData, 0.7, 0.9);
+            NetworkUtil.TestTraining(bprop, 0.01);
+        }
+
+        [TestMethod]
+        public void TestManhattan()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+            MLTrain bprop = new ManhattanPropagation(network, trainingData, 0.01);
+            NetworkUtil.TestTraining(bprop, 0.01);
+        }
+
+        [TestMethod]
+        public void TestSCG()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+            MLTrain bprop = new ScaledConjugateGradient(network, trainingData);
+            NetworkUtil.TestTraining(bprop, 0.04);
+        }
+
+        [TestMethod]
+        public void TestAnneal()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+            ICalculateScore score = new TrainingSetScore(trainingData);
+            NeuralSimulatedAnnealing anneal = new NeuralSimulatedAnnealing(network, score, 10, 2, 100);
+            NetworkUtil.TestTraining(anneal, 0.01);
+        }
+
+        [TestMethod]
+        public void TestGenetic()
+        {
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+            BasicNetwork network = NetworkUtil.CreateXORNetworkUntrained();
+            ICalculateScore score = new TrainingSetScore(trainingData);
+            NeuralGeneticAlgorithm genetic = new NeuralGeneticAlgorithm(network, new RangeRandomizer(-1, 1), score, 500, 0.1, 0.25);
+            NetworkUtil.TestTraining(genetic, 0.00001);
+        }
+
+        [TestMethod]
+        public void TestRegPNN()
+        {
+
+            PNNOutputMode mode = PNNOutputMode.Regression;
+            BasicPNN network = new BasicPNN(PNNKernelType.Gaussian, mode, 2, 1);
+
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            TrainBasicPNN train = new TrainBasicPNN(network, trainingData);
+            train.Iteration();
+
+            XOR.VerifyXOR(network, 0.01);
+        }
+
+        [TestMethod]
+        public void TestClassifyPNN()
+        {
+
+            PNNOutputMode mode = PNNOutputMode.Classification;
+            BasicPNN network = new BasicPNN(PNNKernelType.Gaussian, mode, 2, 2);
+
+            MLDataSet trainingData = new BasicMLDataSet(XOR.XORInput, XOR.XORIdeal);
+
+            TrainBasicPNN train = new TrainBasicPNN(network, trainingData);
+            train.Iteration();
+
+            XOR.VerifyXOR(network, 0.01);
+        }
+    }
+}
---
+++ encog-core-test/Util/NetworkUtil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Engine.Network.Activation;
+using Encog.MathUtil.Randomize;
+using Encog.ML.Train;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Encog.Util
+{
+    public class NetworkUtil
+    {
+        public static BasicNetwork CreateXORNetworkUntrained()
+        {
+            // random matrix data.  However, it provides a constant starting point 
+            // for the unit tests.		
+            BasicNetwork network = new BasicNetwork();
+            network.AddLayer(new BasicLayer(null, true, 2));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, 4));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), false, 1));
+            network.Structure.FinalizeStructure();
+
+            (new ConsistentRandomizer(-1, 1)).Randomize(network);
+
+            return network;
+        }
+
+        public static BasicNetwork CreateXORNetworknNguyenWidrowUntrained()
+        {
+            // random matrix data.  However, it provides a constant starting point 
+            // for the unit tests.
+
+            BasicNetwork network = new BasicNetwork();
+            network.AddLayer(new BasicLayer(null, true, 2));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, 3));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), false, 3));
+            network.AddLayer(new BasicLayer(null, false, 1));
+            network.Structure.FinalizeStructure();
+            (new NguyenWidrowRandomizer(-1, 1)).Randomize(network);
+
+            return network;
+        }
+
+        public static void TestTraining(MLTrain train, double requiredImprove)
+        {
+            train.Iteration();
+            double error1 = train.Error;
+
+            for (int i = 0; i < 10; i++)
+                train.Iteration();
+
+            double error2 = train.Error;
+
+            double improve = (error1 - error2) / error1;
+            Assert.IsTrue(improve >= requiredImprove,"Improve rate too low for " + train.GetType().Name +
+                    ",Improve=" + improve + ",Needed=" + requiredImprove);
+        }
+    }
+}
