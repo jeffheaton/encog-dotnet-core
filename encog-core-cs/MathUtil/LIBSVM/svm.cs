2da73b115976fe84adadee121406e3d339c74ad1
Miley Thomas <MileyThomas87@yahoo.com>
2011-05-20 15:58:32 +0000

    Updated source file headers for 3.0
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@667 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

--- encog-core-cs/MathUtil/LIBSVM/svm.cs
+++ encog-core-cs/MathUtil/LIBSVM/svm.cs
@@ -1,12 +1,25 @@
-/*
- * Conversion notes:
- * Using JLCA 3.0, the only problem was with the save and loads. I changed them both to be
- * StreamR/W around a FileStream. Originally, the save was a BinaryWriter over a FileStream
- * and the Reader was a StreamReader over another StreamReader.
- * In the Java code, it's a DataOutputStream around a FileOutputStream and a BufferedReader
- * around a FileReader.
- */
-
+//
+// Encog(tm) Core v3.0 - .Net Version
+// http://www.heatonresearch.com/encog/
+//
+// Copyright 2008-2011 Heaton Research, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//   
+// For more information on Heaton Research copyrights, licenses 
+// and trademarks visit:
+// http://www.heatonresearch.com/copyright
+//
 using System;
 using System.IO;
 
