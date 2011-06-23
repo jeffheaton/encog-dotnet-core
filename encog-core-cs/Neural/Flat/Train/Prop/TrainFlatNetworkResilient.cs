212ae54d10fdb49d13cff5f47d9bef9a28f47cbe
Jeff Heaton <jeff@jeffheaton.com>
2011-06-23 01:50:27 +0000

    Added all 4 RPROP types.
    git-svn-id: http://encog-cs.googlecode.com/svn/trunk/encog-core@696 0d3f3ea6-ac51-0410-a1de-edb5cd2a854a

--- encog-core-cs/Neural/Flat/Train/Prop/TrainFlatNetworkResilient.cs
+++ encog-core-cs/Neural/Flat/Train/Prop/TrainFlatNetworkResilient.cs
@@ -22,6 +22,7 @@
 //
 using System;
 using Encog.ML.Data;
+using Encog.Neural.Networks.Training;
 
 namespace Encog.Neural.Flat.Train.Prop
 {
@@ -31,6 +32,16 @@ namespace Encog.Neural.Flat.Train.Prop
     ///
     public class TrainFlatNetworkResilient : TrainFlatNetworkProp
     {
+        /// <summary>
+        /// The last deltas.
+        /// </summary>
+        private readonly double[] _lastDelta;
+
+        /// <summary>
+        /// The last weight changed.
+        /// </summary>
+        private readonly double[] _lastWeightChanged;
+
         /// <summary>
         /// The maximum step value for rprop.
         /// </summary>
@@ -50,6 +61,11 @@ namespace Encog.Neural.Flat.Train.Prop
         private readonly double _zeroTolerance;
 
         /// <summary>
+        /// The last error.
+        /// </summary>
+        private double _lastError;
+
+        /// <summary>
         /// Construct a resilient trainer for flat networks.
         /// </summary>
         ///
@@ -65,6 +81,8 @@ namespace Encog.Neural.Flat.Train.Prop
             _updateValues = new double[network.Weights.Length];
             _zeroTolerance = zeroTolerance;
             _maxStep = maxStep;
+            _lastWeightChanged = new double[Network.Weights.Length];
+            _lastDelta = new double[Network.Weights.Length];
 
             for (int i = 0; i < _updateValues.Length; i++)
             {
@@ -85,6 +103,11 @@ namespace Encog.Neural.Flat.Train.Prop
                 RPROPConst.DefaultMaxStep)
         {
         }
+
+        /// <summary>
+        /// The type of RPROP to use.
+        /// </summary>
+        public RPROPType RType { get; set; }
 
         /// <value>The RPROP update values.</value>
         public double[] UpdateValues
@@ -122,6 +145,34 @@ namespace Encog.Neural.Flat.Train.Prop
         public override double UpdateWeight(double[] gradients,
                                             double[] lastGradient, int index)
         {
+            double weightChange = 0;
+
+            switch (RType)
+            {
+                case RPROPType.RPROPp:
+                    weightChange = UpdateWeightPlus(gradients, lastGradient, index);
+                    break;
+                case RPROPType.RPROPm:
+                    weightChange = UpdateWeightMinus(gradients, lastGradient, index);
+                    break;
+                case RPROPType.iRPROPp:
+                    weightChange = UpdateiWeightPlus(gradients, lastGradient, index);
+                    break;
+                case RPROPType.iRPROPm:
+                    weightChange = UpdateiWeightMinus(gradients, lastGradient, index);
+                    break;
+                default:
+                    throw new TrainingError("Unknown RPROP type: " + RType);
+            }
+
+            _lastWeightChanged[index] = weightChange;
+            return weightChange;
+        }
+
+
+        public double UpdateWeightPlus(double[] gradients,
+                                       double[] lastGradient, int index)
+        {
             // multiply the current and previous gradient, and take the
             // sign. We want to see if the gradient has changed its sign.
             int change = Sign(gradients[index]*lastGradient[index]);
@@ -131,19 +182,22 @@ namespace Encog.Neural.Flat.Train.Prop
             // delta so that it will converge faster
             if (change > 0)
             {
-                double delta = _updateValues[index]*RPROPConst.PositiveEta;
+                double delta = UpdateValues[index]
+                               *RPROPConst.PositiveEta;
                 delta = Math.Min(delta, _maxStep);
                 weightChange = Sign(gradients[index])*delta;
-                _updateValues[index] = delta;
+                UpdateValues[index] = delta;
                 lastGradient[index] = gradients[index];
             }
             else if (change < 0)
             {
                 // if change<0, then the sign has changed, and the last
                 // delta was too big
-                double delta = _updateValues[index]*RPROPConst.NegativeEta;
+                double delta = UpdateValues[index]
+                               *RPROPConst.NegativeEta;
                 delta = Math.Max(delta, RPROPConst.DeltaMin);
-                _updateValues[index] = delta;
+                UpdateValues[index] = delta;
+                weightChange = -_lastWeightChanged[index];
                 // set the previous gradent to zero so that there will be no
                 // adjustment the next iteration
                 lastGradient[index] = 0;
@@ -159,6 +213,125 @@ namespace Encog.Neural.Flat.Train.Prop
             // apply the weight change, if any
             return weightChange;
         }
+
+        public double UpdateWeightMinus(double[] gradients,
+                                        double[] lastGradient, int index)
+        {
+            // multiply the current and previous gradient, and take the
+            // sign. We want to see if the gradient has changed its sign.
+            int change = Sign(gradients[index]*lastGradient[index]);
+            double weightChange = 0;
+            double delta;
+
+            // if the gradient has retained its sign, then we increase the
+            // delta so that it will converge faster
+            if (change > 0)
+            {
+                delta = _lastDelta[index]
+                        *RPROPConst.PositiveEta;
+                delta = Math.Min(delta, _maxStep);
+            }
+            else
+            {
+                // if change<0, then the sign has changed, and the last
+                // delta was too big
+                delta = _lastDelta[index]
+                        *RPROPConst.NegativeEta;
+                delta = Math.Max(delta, RPROPConst.DeltaMin);
+            }
+
+            lastGradient[index] = gradients[index];
+            weightChange = Sign(gradients[index])*delta;
+            _lastDelta[index] = delta;
+
+            // apply the weight change, if any
+            return weightChange;
+        }
+
+        public double UpdateiWeightPlus(double[] gradients,
+                                        double[] lastGradient, int index)
+        {
+            // multiply the current and previous gradient, and take the
+            // sign. We want to see if the gradient has changed its sign.
+            int change = Sign(gradients[index]*lastGradient[index]);
+            double weightChange = 0;
+
+            // if the gradient has retained its sign, then we increase the
+            // delta so that it will converge faster
+            if (change > 0)
+            {
+                double delta = _updateValues[index]
+                               *RPROPConst.PositiveEta;
+                delta = Math.Min(delta, _maxStep);
+                weightChange = Sign(gradients[index])*delta;
+                _updateValues[index] = delta;
+                lastGradient[index] = gradients[index];
+            }
+            else if (change < 0)
+            {
+                // if change<0, then the sign has changed, and the last
+                // delta was too big
+                double delta = UpdateValues[index]
+                               *RPROPConst.NegativeEta;
+                delta = Math.Max(delta, RPROPConst.DeltaMin);
+                UpdateValues[index] = delta;
+
+                if (CurrentError > _lastError)
+                {
+                    weightChange = -_lastWeightChanged[index];
+                }
+
+                // set the previous gradent to zero so that there will be no
+                // adjustment the next iteration
+                lastGradient[index] = 0;
+            }
+            else if (change == 0)
+            {
+                // if change==0 then there is no change to the delta
+                double delta = _updateValues[index];
+                weightChange = Sign(gradients[index])*delta;
+                lastGradient[index] = gradients[index];
+            }
+
+            // apply the weight change, if any
+            return weightChange;
+        }
+
+        public double UpdateiWeightMinus(double[] gradients,
+                                         double[] lastGradient, int index)
+        {
+            // multiply the current and previous gradient, and take the
+            // sign. We want to see if the gradient has changed its sign.
+            int change = Sign(gradients[index]*lastGradient[index]);
+            double weightChange = 0;
+            double delta;
+
+            // if the gradient has retained its sign, then we increase the
+            // delta so that it will converge faster
+            if (change > 0)
+            {
+                delta = _lastDelta[index]
+                        *RPROPConst.PositiveEta;
+                delta = Math.Min(delta, _maxStep);
+            }
+            else
+            {
+                // if change<0, then the sign has changed, and the last
+                // delta was too big
+                delta = _lastDelta[index]
+                        *RPROPConst.NegativeEta;
+                delta = Math.Max(delta, RPROPConst.DeltaMin);
+                lastGradient[index] = 0;
+            }
+
+            lastGradient[index] = gradients[index];
+            weightChange = Sign(gradients[index])*delta;
+            _lastDelta[index] = delta;
+
+            // apply the weight change, if any
+            return weightChange;
+        }
+
 
         /// <summary>
         /// Not needed for this training type.
