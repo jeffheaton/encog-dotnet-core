d60e44b9c938baf0fcc4cc76d3265096b7fb8c87
fxmozart <fxmozart@gmail.com>
2011-08-21 22:40:49 +0200

    New
    Signed-off-by: fxmozart <fxmozart@gmail.com>

---
+++ Last/fxmozart-encog-dotnet-core-24b300b/ConsoleExamples/Examples/SuperPredict/marketpredict.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using ConsoleExamples.Examples;
+
+namespace Encog.Examples.RangeAndPredictions
+{
+
+    public class RangeCalculators : IExample
+    {
+        public static ExampleInfo Info
+        {
+            get
+            {
+                var info = new ExampleInfo(
+                    typeof (RangeCalculators),
+                    "csvrange",
+                    "Simple Range Prediction",
+                    "Use csv data to predict ranges of a stock or forex, futures instrument.");
+                return info;
+            }
+        }
+
+        #region IExample Members
+
+        public void Execute(IExampleInterface app)
+        {
+            if (app.Args.Length < 1)
+            {
+                Console.WriteLine(@"csvRange [generate/train/prune/evaluate] PathToFile");
+                Console.WriteLine(@"e.g csvrange [generate/train/prune/evaluate] c:\\EURUSD.csv");
+                Console.WriteLine(@"csvrange train Directory");
+            }
+            else
+            {
+                FileInfo dataDir = new FileInfo(Environment.CurrentDirectory);
+                if (String.Compare(app.Args[0], "generate", true) == 0)
+                {
+                    MarketBuildTraining.Generate(app.Args[1]);
+                }
+                else if (String.Compare(app.Args[0], "train", true) == 0)
+                {
+                    Examples.RangeCalculators.MarketTrain.Train(dataDir);
+                }
+                else if (String.Compare(app.Args[0], "evaluate", true) == 0)
+                {
+                    //MarketEvaluate.Evaluate(dataDir,app.Args[1]);
+                }
+                else if (String.Compare(app.Args[0], "prune", true) == 0)
+                {
+                    {
+                        Examples.RangeCalculators.MarketPrune.Incremental(dataDir);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Last/fxmozart-encog-dotnet-core-24b300b/ConsoleExamples/Examples/SuperPredict/marketprune.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Encog.Engine.Network.Activation;
+using Encog.ML.Data;
+using Encog.Neural.Pattern;
+using Encog.Neural.Prune;
+using Encog.Persist;
+using Encog.Util.File;
+using Encog.Util.Simple;
+
+namespace Encog.Examples.RangeAndPredictions
+{
+
+    public class MarketPrune
+    {
+        public static void Incremental(FileInfo dataDir)
+        {
+            FileInfo file = FileUtil.CombinePath(dataDir, RangeConfig.TRAINING_FILE);
+
+            if (!file.Exists)
+            {
+                Console.WriteLine(@"Can't read file: " + file);
+                return;
+            }
+
+            IMLDataSet training = EncogUtility.LoadEGB2Memory(file);
+    
+
+            var pattern = new FeedForwardPattern
+                              {
+                                  InputNeurons = training.InputSize,
+                                  OutputNeurons = training.IdealSize,
+                                  ActivationFunction = new ActivationTANH()
+                              };
+
+            var prune = new PruneIncremental(training, pattern, 100, 1, 10,
+                                             new ConsoleStatusReportable());
+
+            prune.AddHiddenLayer(5, 50);
+            prune.AddHiddenLayer(0, 50);
+
+            prune.Process();
+
+            EncogDirectoryPersistence.SaveObject(file, prune.BestNetwork);
+        }
+    }
+}
