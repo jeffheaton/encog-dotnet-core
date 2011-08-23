440a4d17fea09a9eb551730d32c8c75481228d0b
fxmozart fxmozart <fxmozart@gmail.com>
2011-08-23 17:24:13 +0200

    Translated from Java the SVM Predict/SVMPredictSunSpots.cs ..
    Runs very smoothly.
    
    Signed-off-by: fxmozart fxmozart <fxmozart@gmail.com>

--- ConsoleExamples/Examples/SVM Predict/CreateSVMNetWork.cs
+++ ConsoleExamples/Examples/SVM Predict/CreateSVMNetWork.cs
@@ -4,8 +4,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Encog.Examples.RangeandMarket;
+using Encog.MathUtil.LIBSVM;
+using Encog.ML;
 using Encog.ML.Data;
 using Encog.ML.Data.Basic;
+using Encog.ML.Factory;
 using Encog.ML.SVM;
 using Encog.ML.SVM.Training;
 using Encog.ML.Train;
@@ -13,6 +16,8 @@ using Encog.ML.Train.Strategy;
 using Encog.Neural.Networks.Training;
 using Encog.Neural.Networks.Training.Anneal;
 using Encog.Neural.Networks.Training.Propagation.Back;
+using Encog.Neural.Networks.Training.Propagation.Manhattan;
+using Encog.Util.Simple;
 using SuperUtils = Encog.Util.NetworkUtil.NetworkUtility;
 namespace Encog.Examples.SVM_Predict
 {
@@ -72,7 +77,6 @@ namespace Encog.Examples.SVM_Predict
             ///  var network = (BasicNetwork)CreateEval.CreateElmanNetwork(SuperSet.InputSize, SuperSet.IdealSize);
             //double error = CreateEval.TrainNetworks(machine, SuperSet);
 
-           
             TrainSVM(train, machine);
 
             //Lets create an evaluation.
@@ -80,13 +84,32 @@ namespace Encog.Examples.SVM_Predict
         }
 
 
+        private static BasicMLDataSet MakeAsets(int inputs , int predictWindow)
+        {
+             double[] firstinput = MakeInputs(inputs);
+            double[] SecondInput = MakeInputs(inputs);
+            double[] ThirdInputs = MakeInputs(inputs);
+            double[] FourthInputs = MakeInputs(inputs);
+            var pair = SuperUtils.ProcessPair(firstinput, FourthInputs, inputs, predictWindow);
+            var pair2 = SuperUtils.ProcessPair(SecondInput, FourthInputs, inputs, predictWindow);
+            var pair3 = SuperUtils.ProcessPair(ThirdInputs, FourthInputs, inputs, predictWindow);
+            var pair4 = SuperUtils.ProcessPair(FourthInputs, FourthInputs, inputs, predictWindow);
+            BasicMLDataSet SuperSet = new BasicMLDataSet();
+            SuperSet.Add(pair);
+            SuperSet.Add(pair2);
+            SuperSet.Add(pair3);
+            SuperSet.Add(pair4);
+
+            return SuperSet;
+        }
+
+
+
         public static double TrainSVM(SVMTrain train, SupportVectorMachine machine)
         {
            
             StopTrainingStrategy stop = new StopTrainingStrategy(0.0001, 200);
             train.AddStrategy(stop);
-
-
             var sw = new Stopwatch();
             sw.Start();
             while (!stop.ShouldStop())
@@ -101,5 +124,32 @@ namespace Encog.Examples.SVM_Predict
             Console.WriteLine(@"SVM Trained in :" + sw.ElapsedMilliseconds);
             return train.Error;
         }
+
+
+        public static void Process(String methodName, String methodArchitecture, String trainerName, String trainerArgs,
+                            int outputNeurons)
+        {
+            // first, create the machine learning method
+            var methodFactory = new MLMethodFactory();
+            IMLMethod method = methodFactory.Create(methodName, methodArchitecture, 2, 1);
+            // second, create the data set		
+            IMLDataSet dataSet = MakeAsets(3000, outputNeurons);
+            // third, create the trainer
+            var trainFactory = new MLTrainFactory();
+            IMLTrain train = trainFactory.Create(method, dataSet, trainerName, trainerArgs);
+            // reset if improve is less than 1% over 5 cycles
+            if (method is IMLResettable && !(train is ManhattanPropagation))
+            {
+                train.AddStrategy(new RequiredImprovementStrategy(500));
+            }
+            // fourth, train and evaluate.
+            EncogUtility.TrainToError(train, 0.01);
+            EncogUtility.Evaluate((IMLRegression)method, dataSet);
+
+            // finally, write out what we did
+            Console.WriteLine(@"Machine Learning Type: " + methodName);
+            Console.WriteLine(@"Machine Learning Architecture: " + methodArchitecture);
+            Console.WriteLine(@"Training Method: " + trainerName);
+            Console.WriteLine(@"Training Args: " + trainerArgs);}
     }
 }
--- ConsoleExamples/Examples/SVM Predict/SVMExample.cs
+++ ConsoleExamples/Examples/SVM Predict/SVMExample.cs
@@ -4,11 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using ConsoleExamples.Examples;
+using Encog.ML.Factory;
 
 namespace Encog.Examples.SVM_Predict
 {
     class SVMExample :IExample
     {
+        public const string METHOD_FEEDFORWARD_A = "?:B->SIGMOID->4:B->SIGMOID->?";
+        public const string METHOD_BIASLESS_A = "?->SIGMOID->4->SIGMOID->?";
+        public const string METHOD_SVMC_A = "?->C->?";
+        public const string METHOD_SVMR_A = "?->R->?";
+        public const string METHOD_RBF_A = "?->gaussian(c=4)->?";
+        public const string METHOD_PNNC_A = "?->C(kernel=gaussian)->?";
+        public const string METHOD_PNNR_A = "?->R(kernel=gaussian)->?";
+
+
 
         public static ExampleInfo Info
         {
@@ -32,19 +42,36 @@ namespace Encog.Examples.SVM_Predict
         {
             this.app = app;
             FileInfo dataDir = new FileInfo(Environment.CurrentDirectory);
+            if (String.Compare(app.Args[0], "sun", true) == 0)
+            {
 
+                SVMPredictSunSpots.PredictSunspotSVM.run();
+                MakeAPause();
+                app.Exit();
+            }
+            if (String.Compare(app.Args[0], "svm", true) == 0)
+            {
+                
+               CreateSVMNetWork.Process(
+               MLMethodFactory.TypeSVM,
+               METHOD_SVMR_A,
+               MLTrainFactory.TypeSVM,
+               "", 1);
+            }
             if (String.Compare(app.Args[0], "random", true) == 0)
             {
                 if (app.Args.Length > 1)
                 {
                     SVM_Predict.CreateSVMNetWork.RandomTrainerMethod(Convert.ToInt16(app.Args[1]), Convert.ToInt16(app.Args[2]));
-
+                    MakeAPause();
+                    app.Exit();
                 }
                 else
                 {
                     Console.WriteLine(@"You didn't input enough args in your request, will default to 3000 inputs , and 50 prediction size");
-                    SVM_Predict.CreateSVMNetWork.RandomTrainerMethod(3000, 50);
+                    SVM_Predict.CreateSVMNetWork.RandomTrainerMethod(3000, 1);
                     MakeAPause();
+                    app.Exit();
                 }
 
             }
