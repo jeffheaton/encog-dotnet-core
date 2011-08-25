498949655f3586046c3f17092ae75d4925d06455
fxmozart fxmozart <fxmozart@gmail.com>
2011-08-26 01:30:16 +0200

    90% done translating the SP 500 example for 3.0

---
+++ ConsoleExamples/Examples/RateSP500/InterestRate.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog.Examples.RateSP500
+{
+    public class InterestRate : IComparable<InterestRate>
+    {
+        private DateTime effectiveDate;
+        private double rate;
+
+        public InterestRate(DateTime effectiveDate, double rate)
+        {
+            this.effectiveDate = effectiveDate;
+            this.rate = rate;
+        }
+
+        public int CompareTo(InterestRate other)
+        {
+            return getEffectiveDate().CompareTo(other.getEffectiveDate());
+        }
+
+        /**
+         * @return the effectiveDate
+         */
+        public DateTime getEffectiveDate()
+        {
+            return this.effectiveDate;
+        }
+
+        /**
+         * @return the rate
+         */
+        public double getRate()
+        {
+            return this.rate;
+        }
+
+        /**
+         * @param effectiveDate
+         *            the effectiveDate to set
+         */
+        public void setEffectiveDate(DateTime effectiveDate)
+        {
+            this.effectiveDate = effectiveDate;
+        }
+
+        /**
+         * @param rate
+         *            the rate to set
+         */
+        public void setRate(double rate)
+        {
+            this.rate = rate;
+        }
+    }
+}
