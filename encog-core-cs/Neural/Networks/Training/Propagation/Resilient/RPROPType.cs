ac5bb88ed2c9cff8b501561bffefc8fe3e6344e7
Jeff Heaton <jeff@jeffheaton.com>
2011-08-26 10:40:08 -0500

    Adding files that should have been in the last commit :(

---
+++ encog-core-cs/Neural/Networks/Training/Propagation/Resilient/RPROPType.cs
@@ -0,0 +1,33 @@
+﻿namespace Encog.Neural.Networks.Training.Propagation.Resilient
+{
+    /// <summary>
+    /// Allows the type of RPROP to be defined.  RPROPp is the classic RPROP.
+    /// 
+    /// For more information, visit:
+    /// 
+    /// http://www.heatonresearch.com/wiki/RPROP
+    /// </summary>
+    public enum RPROPType
+    {
+        /// <summary>
+        /// RPROP+ : The classic RPROP algorithm.  Uses weight back tracking.
+        /// </summary>
+        RPROPp,
+
+        /// <summary>
+        /// RPROP- : No weight back tracking.
+        /// </summary>
+        RPROPm,
+
+        /// <summary>
+        /// iRPROP+ : New weight back tracking method, some consider this to be
+        /// the most advanced RPROP.
+        /// </summary>
+        iRPROPp,
+
+        /// <summary>
+        /// iRPROP- : New RPROP without weight back tracking. 
+        /// </summary>
+        iRPROPm
+    }
+}
