896625ba1abed8ee2c490485597bacb5aa30c266
Jeff Heaton <jeff@jeffheaton.com>
2012-03-10 14:06:42 -0600

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
+md \stage\encog\encog-dotnet-dll-3.1.0
+md \stage\encog\encog-dotnet-core-3.1.0
+
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild encog-core-cs.sln /property:Configuration=Release
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild encog-core.shfbproj
 
-md \stage\encog\encog-dotnet-dll-3.0.1
-md \stage\encog\encog-dotnet-core-3.0.1
 
-copy C:\Users\jheaton\dotnet\build\encog-dotnet-core\EncogCmd\bin\Release\EncogCmd.exe c:\stage\encog\encog-dotnet-dll-3.0.1
-copy C:\Users\jheaton\dotnet\build\encog-dotnet-core\encog-core-cs\bin\Release\encog-core-cs.dll c:\stage\encog\encog-dotnet-dll-3.0.1
-copy C:\Users\jheaton\dotnet\build\encog-dotnet-core\encog-core-cs\bin\Release\encog-core-cs.xml c:\stage\encog\encog-dotnet-dll-3.0.1
-copy C:\Users\jheaton\dotnet\build\encog-dotnet-core\encog-core.chm c:\stage\encog\encog-dotnet-dll-3.0.1
-copy C:\Users\jheaton\dotnet\build\encog-dotnet-core\LICENSE.TXT c:\stage\encog\encog-dotnet-dll-3.0.1
-copy C:\Users\jheaton\dotnet\build\encog-dotnet-core\README.TXT c:\stage\encog\encog-dotnet-dll-3.0.1
+copy .\encog-core-cs\bin\Release\encog-core-cs.dll c:\stage\encog\encog-dotnet-dll-3.1.0
+copy .\encog-core-cs\bin\Release\encog-core-cs.xml c:\stage\encog\encog-dotnet-dll-3.1.0
+copy .\encog-dotnet-core\encog-core.chm c:\stage\encog\encog-dotnet-dll-3.1.0
+copy .\LICENSE.TXT c:\stage\encog\encog-dotnet-dll-3.1.0
+copy .\README.TXT c:\stage\encog\encog-dotnet-dll-3.1.0
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild encog-core-cs.sln /t:clean
 
-XCOPY . c:\stage\encog\encog-dotnet-core-3.0.1\ /s /i
+XCOPY . c:\stage\encog\encog-dotnet-core-3.1.0\ /s /i
