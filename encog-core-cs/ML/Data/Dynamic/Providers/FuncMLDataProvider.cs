046c0e2c7d8ea119659a8bd5722d13e1b5194c4c
Brannon King <countprimes@gmail.com>
2012-04-10 20:10:03 -0600

    Added sliding window and dynamic dataset

---
+++ encog-core-cs/ML/Data/Dynamic/Providers/FuncMLDataProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog.ML.Data.Dynamic
+{
+	/// <summary>
+	/// Used for the input parameters to the sliding window dataset.
+	/// </summary>
+	public class FuncMLDataProvider: IDynamicMLDataProvider
+	{
+		Func<int, int, double> _handler;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="handler">A method that takes as input the input/ideal sample and offset into that sample.</param>
+		/// <param name="count">Total number of elements in this dataset.</param>
+		/// <param name="size">The number of items per sample. eg. size of the input/output layer.</param>
+		public FuncMLDataProvider(Func<int, int, double> handler, int count, int size)
+		{
+			if(handler == null) throw new ArgumentNullException("handler");
+			if(count < 1) throw new ArgumentException("Value is too small.", "count");
+			if(size < 1) throw new ArgumentException("Value is too small.", "size");
+			
+			_handler = handler;
+			Count = count;
+			Size = size;
+		}
+
+		public int Count
+		{
+			get;
+			protected set;
+		}
+
+		public int Size
+		{
+			get;
+			protected set;
+		}
+
+		public double this[int chunk, int index]
+		{
+			get { return _handler.Invoke(chunk, index); }
+		}
+	}
+}
---
+++ encog-core-cs/ML/Data/Dynamic/Providers/SlidingWindowMLDataProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog.ML.Data.Dynamic
+{
+	/// <summary>
+	/// Used for the input parameters to the sliding window dataset.
+	/// </summary>
+	public class SlidingWindowMLDataProvider: IDynamicMLDataProvider
+	{
+		public readonly IList<double> List;
+		public readonly int WindowSize, WindowOffset, StepSize;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="list">List of data to be used as input or ideal.</param>
+		/// <param name="windowSize">Size of Input/Ideal.</param>
+		/// <param name="windowOffset">Shift +/- for a given index.</param>
+		/// <param name="stepSize">How much we move by for each discrete index.</param>
+		public SlidingWindowMLDataProvider(IList<double> list, int windowSize, int windowOffset, int stepSize = 1)
+		{
+			if(list == null) throw new ArgumentNullException("list");
+			if(list.Count < 2) throw new ArgumentException("List is too small.", "list");
+			if(stepSize < 1) throw new ArgumentException("Value is too small.", "stepSize");
+			if(windowSize < 1) throw new ArgumentException("Value is too small.", "windowSize");
+
+			List = list;
+			WindowSize = windowSize;
+			WindowOffset = windowOffset;
+			StepSize = stepSize;
+		}
+
+		public int Count
+		{
+			get { return List.Count / StepSize; }
+		}
+
+		public int Size
+		{
+			get { return WindowSize; }
+		}
+
+		/// <summary>
+		/// If true, the list boundaries are extended when the window takes us beyond the boundary.
+		/// </summary>
+		public bool PadWithNearest { get; set; }
+
+		/// <summary>
+		/// Defaults to 0.0
+		/// </summary>
+		public double DefaultPadValue { get; set; }
+
+		public double this[int chunk, int index]
+		{
+			get 
+			{
+				var offset = chunk * StepSize + index + WindowOffset;
+				if(offset < 0) return PadWithNearest ? List.First() : DefaultPadValue;
+				if(offset >= List.Count) return PadWithNearest ? List.Last() : DefaultPadValue;
+				return List[offset];
+			}
+		}
+	}
+}
